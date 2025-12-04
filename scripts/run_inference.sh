874059a7c5378befb29a3eb35bcee1c03e45d2b2
unolop <yparkaa@korea.ac.kr>
2025-12-04 21:40:12 +0900

    reorganizing

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # https://swift.readthedocs.io/en/latest/Instruction/Supported-models-and-datasets.html 
 
-GPU=1
-DATASET_PATHS=("vqa_1k"  "vqa_5k" ) # "mmstar" "spubench" 
+GPU=0
+DATASET_PATHS=("mmstar" "spubench" ) # "vqa_1k"  "vqa_5k" 
 CONDITION_LIST=("_blind" ) #  "_sys_inst_blind" "_inst_blind" "" 
 BASE_MODEL=(
-    # "Qwen/Qwen3-VL-2B-Instruct"
+    "Qwen/Qwen3-VL-2B-Instruct"
     # "llava-hf/llava-v1.6-vicuna-7b-hf"
     # "llava-hf/llava-1.5-7b-hf"
-    "OpenGVLab/InternVL3_5-4B"
+    # "OpenGVLab/InternVL3_5-4B"
     # "OpenGVLab/InternVL3_5-2B"
     # "OpenGVLab/InternVL3_5-1B"
-    "OpenGVLab/InternVL3_5-8B"
+    # "OpenGVLab/InternVL3_5-8B"
     
     ## 15gb 
-    "llava-hf/llava-v1.6-mistral-7b-hf"
+    # "llava-hf/llava-v1.6-mistral-7b-hf"
     # "OpenGVLab/InternVL3_5-8B"
     # "Qwen/Qwen3-VL-8B-Instruct"
-    "Qwen/Qwen3-VL-4B-Instruct"
+    # "Qwen/Qwen3-VL-4B-Instruct"
 
             )
 # ADAPTERS_PATHS=(
@@ -32,7 +32,8 @@ for BASE_MODEL in "${BASE_MODEL[@]}"; do
                 --model "${BASE_MODEL}" \
                 --dataset "${VAL_DATASET}" \
                 --condition "${CONDITION}" \
-                --gpu "${GPU}" 
+                --gpu "${GPU}" \
+                --resume
         done 
     done
 done 
