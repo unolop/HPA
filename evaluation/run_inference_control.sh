ea0d5806481a9f5d4c4b43bf5a073eb7fbd70457
unolop <yparkaa@korea.ac.kr>
2026-02-24 21:52:44 +0900

    yuna

--- evaluation/run_inference_control.sh
+++ evaluation/run_inference_control.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://swift.readthedocs.io/en/latest/Instruction/Supported-models-and-datasets.html 
 
-GPU=1
+GPU=0
 CONDITION_LIST=("_inst_blind" ) #  "_sys_inst_blind" "_blind" ""
 BASE_MODEL=(
     # "OpenGVLab/InternVL3_5-1B"
@@ -14,8 +14,8 @@ BASE_MODEL=(
     # "llava-hf/llava-1.5-7b-hf"
     # "llava-hf/llava-v1.6-mistral-7b-hf"
     # "OpenGVLab/InternVL3_5-8B"
-    "Qwen/Qwen3-VL-8B-Instruct" 
-    # "Qwen/Qwen3-VL-4B-Instruct"
+    # "Qwen/Qwen3-VL-8B-Instruct" 
+    "Qwen/Qwen3-VL-4B-Instruct"
     # "OpenGVLab/InternVL3_5-4B"
     )
 
