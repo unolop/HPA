d3c18fe7b37a040e4155c5f7c51215d7699be57b
David Lee <shlee_2@korea.ac.kr>
2026-02-24 20:10:43 +0900

    schemas

---
+++ evaluation/inference_control.py
@@ -0,0 +1,134 @@
+import os 
+import re 
+from tqdm import tqdm
+import json
+import numpy as np 
+import torch 
+
+seed = 42
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.cuda.empty_cache() 
+
+
+image_dir_path="/home/david/Desktop/yuna/HPA/data/val2014" 
+json_path="/home/david/Desktop/yuna/HPA/dataset/vqav2_1k_val.json" 
+
+def format_prompt(question): 
+    return f"Question: {question} Answer the question using a single word or phrase. \nAnswer:" 
+ 
+def load_dataset(data_name:str, prompt:str=''): 
+    print("Loading dataset...") 
+    if data_name == "vqa_1k": 
+        from dataset.vqav2 import VQADataset_json
+        dataset = VQADataset_json(prompt=prompt)
+        
+    return dataset 
+
+def main(args):
+
+    from swift.llm import (
+        PtEngine, RequestConfig, safe_snapshot_download, get_model_tokenizer, get_template, InferRequest
+    )
+    from swift.tuners import Swift
+    # Please adjust the following lines
+    model = args.model
+
+    template_type = None  # None: use the default template_type of the corresponding model
+    default_system = None  # None: use the default system prompt of the corresponding model
+
+    # Load model and dialogue template
+    model, tokenizer = get_model_tokenizer(model, use_hf=True) #, max_pixels=448)
+    if args.lora_path is not None:
+        lora_checkpoint = safe_snapshot_download(args.lora_path)  # Change to your checkpoint_dir
+        model = Swift.from_pretrained(model, lora_checkpoint)
+    model.eval()
+    template_type = template_type or model.model_meta.template
+    template = get_template(template_type, tokenizer, default_system=default_system)
+    engine = PtEngine.from_model_template(model, template, max_batch_size=1)
+    request_config = RequestConfig(max_tokens=args.max_token_length, temperature=0)
+    
+    save_name = args.model.split('/')[-1]
+    savedir = args.savedir 
+    if args.lora_path is not None : 
+        finetuned = args.lora_path.split('/')[-4] 
+        finetuned += args.lora_path.split('/')[-3] 
+        save_name += f'/{finetuned}' 
+        savedir += '/finetuned'    
+    else: 
+        savedir += '/pretrained'     
+
+    output_jsonl_path = f"{savedir}/{save_name}/{args.dataset}{args.condition}.jsonl" 
+    prompt= ''
+    if 'inst' in args.condition: 
+        prompt = f"\nNote: No images are provided. For each question, imagine an appropriate image exists and answer based on the most common or universal scenario.\n"
+    
+    dataset = load_dataset(args.dataset, prompt)  
+    os.makedirs(os.path.dirname(output_jsonl_path), exist_ok=True)
+    
+    with open(output_jsonl_path, 'w', encoding='utf-8') as f:
+        
+        for i, data in enumerate(tqdm(dataset)): # for i in tqdm(range(len(dataset))): 
+            data['pid'] = i 
+            prompt = data['question']       
+ 
+            if 'blind' in args.condition: 
+                data['image'] = "/home/work/yuna/HPA/data/blank_224.png"
+                prompt = format_prompt(prompt)
+                
+            messages = [] 
+            
+            if args.model_type == 'vlm':  
+                messages.append({'role': 'user', 'content': f'<image>{prompt}' })
+                try: 
+                    infer_request = InferRequest(
+                        messages=messages,
+                        images=[data['image']] 
+                    )
+                except Exception as e: 
+                    continue
+
+            else : 
+                messages.append({'role': 'user', 'content': prompt})
+                infer_request = InferRequest(
+                    messages=messages 
+                ) 
+
+            try: 
+                resp_list = engine.infer([infer_request], request_config)
+                output_text = resp_list[0].choices[0].message.content 
+            
+                matches = re.findall(r"Answer\s*:?\s*(.+)", output_text)
+                if matches:
+                    output_text = matches[-1].strip().replace('*', '')
+                else:
+                    output_text = output_text.strip().replace('*', '')
+ 
+                data['output'] = output_text 
+                data['question'] = prompt 
+                print('Q:', prompt, 'Output:', data['output'], output_jsonl_path)
+
+                data.pop('image')
+                f.write(json.dumps(data, ensure_ascii=False) + '\n')
+                f.flush()  # 버퍼를 비워 파일에 즉시 쓰도록 강제
+            except Exception as e: 
+                continue
+
+    print(f"모든 작업 완료. 결과가 '{output_jsonl_path}'에 저장되었습니다.")
+
+if __name__ == "__main__":
+    import argparse 
+    parser = argparse.ArgumentParser(description="VQA Evaluation") 
+    parser.add_argument("--model", type=str, default="OpenGVLab/InternVL3_5-2B", help="Model name") 
+    parser.add_argument("--model_type", type=str, default="vlm")
+    parser.add_argument("--resume", action="store_true") 
+    parser.add_argument("--max_token_length", default=512) 
+    parser.add_argument("--lora_path", type=str, default=None, help="LoRA Path") 
+    parser.add_argument("--dataset", type=str, default="mmstar", help="Dataset name") 
+    parser.add_argument('--checkpoint', type=str, default=None, help='Pretrained checkpoint') 
+    parser.add_argument('--savedir', type=str, default="/home/work/yuna/HPA/evaluation/results", help='Save directory of inference') 
+    parser.add_argument("--condition", type=str, default='')
+    
+    args = parser.parse_args() 
+    main(args) 
